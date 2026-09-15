c1a9d1cdd39a2383ddbcfdaf919c74f271a0d362
Syed Yaseen Jamal <yaseenjamal.70.syj@gmail.com>
2026-09-15 12:34:00 +0500

    Changed some config

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,6 @@
+import * as dotenv from 'dotenv';
+dotenv.config();
+
 import { NestFactory } from '@nestjs/core';
 import { NestExpressApplication } from '@nestjs/platform-express';
 import { WsAdapter } from '@nestjs/platform-ws';
